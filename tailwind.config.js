6d1cf6fd56697bf737d659cf292b43ea2fd745c6
hanifsyabani <mh070940@gmail.com>
2024-02-09 07:42:19 +0700

    buat hero section

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'primary' : '#FFF4D4',
+        'secondary' :'#0E6FFF'
+      }
+    },
+  },
+  plugins: [],
+}
+
